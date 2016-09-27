d2f351df9248026b2f9990982a1b45c9e9cb6fa6
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2016-09-27 18:54:21 -0400

    Create default.php

---
+++ default.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>dGRAMOP Development</title>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/jquery.fullPage.css" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/vendors/jquery.easings.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/vendors/scrolloverflow.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.8.4/jquery.fullPage.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+</head>
+<body>
+  <div id="fullpage">
+    <div style="background-color:#81F5FD;" class="section">
+      <center><h1>dGRAMOP Development</h1></center>
+      <center><small>Efficient - Cost-effective - Simple</small></center>
+      <img style="position: absolute; top: 70%; left:45%;" height="100" src="http://cimakids.net/images/scroll%20down.gif?crc=353735164"/>
+    </div>
+    <div style="background-color:turquoise;" class="section">
+      <center><h3>Why Us?</h3></center>
+<center><div id="wideo-embed-block"
+	wideo-width="1080"
+	wideo-height="620"
+	wideo-id="4150151472528941821"
+	autoplay="false">
+</div>
+<script type="text/javascript">(function(){var x=document.createElement('script'), s=document.getElementsByTagName('script')[0];x.async=true;x.src='http://static.wideo.co/js/embed/wideoembed.js';s.parentNode.insertBefore(x,s)})();</script>
+
+    </center>
+    </div>
+    <div style="background-color:#F38D8D;" class="section">
+      <center><h3>Portfolio</h3></center>
+      <center><small>A grid of some of my recent projects. Note that some entries are omitted due some customer requesting that their data is not published. Some projects are also omitted due to their lack of notability.</h3></small><br/>
+      <table>
+        <thead>
+          <tr>
+              <th data-field="name">Customer(s)</th>
+              <th data-field="type">Item type</th>
+              <th data-field="desc">Desc.</th>
+              <th data-field="prname">Project Name</th>
+              <th data-field="link">Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>SnipezzCraft</td>
+            <td>Custom Dynamic Website</td>
+            <td>Fully custom dynamic website with callbacks and hooks to the main server. Material based. Ongoing buisness, returning customer.</td>
+            <td>SnipezzCraft website for the SnipezzCraft server</td>
+            <td>http://snipezzcraft.com</td>
+          </tr>
+          <tr>
+            <td>"Craft.tv"</td>
+            <td>Custom Dynamic Webapp</td>
+            <td>This project is being done for free, our of goodwill</td>
+            <td>"Craft.tv"</td>
+            <td>(Omitted)</td>
+          </tr>
+          <tr>
+            <td>HangHangHappy (AKA HappyCreeper, Hang, HangCreeper, miningman)</td>
+            <td>Custom Semi-Static Website</td>
+            <td>Fully custom static website with PHPbb forums.</td>
+            <td>Miningman website & forums for the miningman server</td>
+            <td>Hosting discontinued, customer failed to pay. (http://miningman.org)</td>
+          </tr>
+          <tr>
+            <td>System31 AKA Oliver Mills</td>
+            <td>Custom plugin for spigot/bukkit 1.8</td>
+            <td>Simple plugin development</td>
+            <td>SignKitPVP</td>
+            <td>Customer failed to pay, but proceeded to install and use plugin</td>
+          </tr>
+          <tr>
+            <td>John Frosch</td>
+            <td>Custom plugin for spigot/bukkit 1.9</td>
+            <td>Simple administrative plugin development</td>
+            <td>staffmode</td>
+            <td>Customer failed to pay in full, but ended on good terms</td>
+          </tr>
+          <tr>
+            <td>Sebatian Dalbjørn</td>
+            <td>Custom plugin for spigot/bukkit 1.8</td>
+            <td>Plugin development</td>
+            <td>pvpplugin</td>
+            <td>Customer used plugin, server was cancelled due to poor financing.</td>
+          </tr>
+          <tr>
+            <td>Sebatian Dalbjørn</td>
+            <td>Custom plugin for spigot/bukkit 1.8</td>
+            <td>Plugin development</td>
+            <td>Arenachest</td>
+            <td>Customer used plugin, server was cancelled due to poor financing.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div style="background-color:#C0F38D;" class="section">
+      <center><h3>Services</h3></center>
+      <div class="row">
+
+        <div class="col s12">
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="http://d3gnp09177mxuh.cloudfront.net/tech-page-images/java.png">
+              <span class="card-title blue">Java Development</span>
+            </div>
+            <div class="card-content">
+              <p>We can provide java development for standalone applications, or plugins for applications like spigot. We support most major libraries, API's, and platforms</p>
+            </div>
+            <div class="card-action">
+              <a href="#faq">Purchase</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12">
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="http://www.frecosse.com/images/slider/website_design.png">
+              <span class="card-title blue">Web Development</span>
+            </div>
+            <div class="card-content">
+              <p>We provide custom web development - both front-end and server-side. Known for our exceptional server side development, we can provide your new website with unparalelled performance and new features</p>
+            </div>
+            <div class="card-action">
+              <a href="#faq">Purchase</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12">
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="https://s18.postimg.org/94dpksxd5/147250725288966_2.png">
+              <span class="card-title blue">Other Services</span>
+            </div>
+            <div class="card-content">
+              <p>I can render many other services ranging from embedded electonics to project management. If I cannot render a service that you need, I can help you find someone who will.</p>
+            </div>
+            <div class="card-action">
+              <a href="#faq">Purchase</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div style="background-color:#F38DE2;" class="section">
+      <center><h3>FAQ</h3></center>
+      <center><b>Q:</b>How much experience do you have?<br/></center>
+      <center><b>A:</b><?php echo date("Y")-2010; ?> years experience in web development.</center><br/><br/>
+      <center><b>Q:</b>How much do your services cost?<br/></center>
+      <center><b>A:</b>Well, it depends on what you need. Truly custom development won't have a steady cost across all services. Contact us for a free estimate</center><br/><br/>
+      <center><b>Q:</b>How can I trust you?<br/></center>
+      <center><b>A:</b>Payment occurs via reversable methods, like PayPal. PayPal allows you to force me to give you a refund, if you can't just talk it out with us. Our goal is 200% customer satisfaction: the product should be what you wanted, what you asked for, what you needed, and what your users needed.</center><br/><br/>
+      <center><b>Q:</b>How can I contact you?<br/></center>
+      <center><b>A:</b>dhruv.gram on skype, or dgramopadhye@gmail.com . Other methods like PMing on forums will take longer to respond</center><br/><br/>
+      <center><b>Q:</b>Awesome! You seem pretty legit. One last thing though. Do you have a linkedin?<br/></center>
+      <center><b>A:</b>Yep: <a href="https://www.linkedin.com/in/dhruv-gramopadhye-6170a3106">Dhruv Gramopadhye on LinkedIn</a></center><br/><br/>
+      <br/><hr/><h5>For any other questions, or buisness requests, feel free to contact dgramopadhye@gmail.com and/or dhruv.gram on skype.</h5>
+
+    </div>
+  </div>
+  <script>
+  $('#fullpage').fullpage({
+    anchors: ['home', 'skills', 'portfolio', 'services', 'faq'],
+    menu: '#bar',
+    afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+
+            if(index == 1){
+              document.getElementsByTagName("title")[0].innerHTML="dGRAMOP Development"
+            }
+            if(index == 2){
+              document.getElementsByTagName("title")[0].innerHTML="dGRAMOP - Skills"
+            }
+            if(index == 3){
+              document.getElementsByTagName("title")[0].innerHTML="dGRAMOP - Portfolio"
+            }
+
+            if(index == 4){
+              document.getElementsByTagName("title")[0].innerHTML="dGRAMOP - Services"
+            }
+
+            if(index == 5){
+              document.getElementsByTagName("title")[0].innerHTML="dGRAMOP - FAQ"
+            }
+
+            if(index == 6){
+              document.getElementsByTagName("title")[0].innerHTML="dGRAMOP - Contact"
+            }
+
+            //Add video for the dGRAMOP Process
+            //Compare us with others
+          }
+});
+</script>
+</body>
+</html>
